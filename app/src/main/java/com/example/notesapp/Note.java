04c20c990887c5d7af2935c8746f43eb3576ec05
anna <anna@mapillary.com>
2018-03-16 09:02:23 +0100

    add model class note

---
+++ app/src/main/java/com/example/notesapp/Note.java
@@ -0,0 +1,31 @@
+package com.example.notesapp;
+
+/**
+ * Created by anna on 3/16/18.
+ */
+
+public class Note {
+    private String title;
+    private String content;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
